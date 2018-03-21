63fe0a89c73469d67cd5d080fa2799017276ff7c
王涛 <desl0451@126.com>
2018-03-21 22:52:33 +0800

    2018-03-21 22：52更新

---
+++ src/org/agent/service/role/RoleService.java
@@ -0,0 +1,49 @@
+package org.agent.service.role;
+
+import java.util.List;
+
+import org.agent.pojo.Role;
+
+public interface RoleService {
+	/**
+	 * 查询所有的角色
+	 */
+	public List<Role> getRoleList();
+
+	/**
+	 * 查询单个角色
+	 * 
+	 * @param role
+	 * @return
+	 */
+	public Role getRole(Role role);
+
+	/**
+	 * 添加
+	 * 
+	 * @param role
+	 * @return
+	 */
+	public int addRole(Role role);
+
+	/**
+	 * 修改
+	 * 
+	 * @param role
+	 * @return
+	 */
+	public int modifyRole(Role role);
+
+	/**
+	 * 删除
+	 * 
+	 * @param role
+	 * @return
+	 */
+	public int deleteRole(Role role);
+	/**
+	 * 仅查询ID和Name
+	 * @return
+	 */
+	public List<Role> getRoleIdAndRnameList(Role role);
+}
---
+++ src/org/agent/service/role/RoleServiceImpl.java
@@ -0,0 +1,51 @@
+package org.agent.service.role;
+
+import java.util.List;
+
+import org.agent.dao.role.RoleMapper;
+import org.agent.pojo.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("roleService")
+public class RoleServiceImpl implements RoleService {
+	@Autowired
+	private RoleMapper mapper;
+
+	@Override
+	public List<Role> getRoleList() {
+		// TODO Auto-generated method stub
+		return mapper.getRoleList();
+	}
+
+	@Override
+	public Role getRole(Role role) {
+		// TODO Auto-generated method stub
+		return mapper.getRole(role);
+	}
+
+	@Override
+	public int addRole(Role role) {
+		// TODO Auto-generated method stub
+		return mapper.addRole(role);
+	}
+
+	@Override
+	public int modifyRole(Role role) {
+		// TODO Auto-generated method stub
+		return mapper.modifyRole(role);
+	}
+
+	@Override
+	public int deleteRole(Role role) {
+		// TODO Auto-generated method stub
+		return mapper.deleteRole(role);
+	}
+
+	@Override
+	public List<Role> getRoleIdAndRnameList(Role role) {
+		// TODO Auto-generated method stub
+		return mapper.getRoleIdAndRnameList(role);
+	}
+
+}
